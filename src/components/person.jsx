67f50a68ff7e5c5e77ab7e60ab845568511d4d70
Evgenii Baldin <evgenii.baldin0@gmail.com>
2025-11-27 19:31:37 -0500

    Added different font for name and centered

--- src/components/person.jsx
+++ src/components/person.jsx
@@ -3,12 +3,18 @@ import React from 'react'
 export default function Person({ person }) {
     return (
         <div style={{ border: '1px solid #ccc', padding: '5px', margin: '10px', width: '250px', width: '80%', backgroundColor: 'white' }}>
-            <img 
-                src={person.picture.large} 
+            <img
+                src={person.picture.large}
                 alt="User"
-                style={{ width: '100%'}}
+                style={{ width: '100%' }}
             />
-            <h3>{person.name.first} {person.name.last}</h3>
+            <div className = 'name' style={{
+                fontSize: '18px',
+                fontWeight: '600',
+                textAlign: 'center'
+            }}>
+                {person.name.first} {person.name.last}
+            </div>
             <p>{person.email}</p>
             <p>{person.location.city} {person.nat}</p>
         </div>
